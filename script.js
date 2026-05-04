99662bf0fbe123a8f279f02c38c31e87e35a5d04
0315233003-lgtm <0315233003@s.tusd1.org>
2026-05-04 18:33:00 +0000

    process

--- script.js
+++ script.js
@@ -17,61 +17,77 @@ function showAllCards() {
     }
 }
 
-function hiddenrecipeCards (recipe) {
-    // Create the main card container
+let hiddenRecipes = [
+    {
+        title: "Birria Tacos",
+        rating: "4.9",
+        image: "birriatacos.jpeg"
+    }
+];
+
+function createHiddenRecipeCard(recipe) {
     let newCard = document.createElement('div');
     newCard.className = 'recipe-card hidden-recipe';
     
-    // Create the image element
     let recipeImage = document.createElement('img');
     recipeImage.src = recipe.image;
     recipeImage.alt = recipe.title;
-    recipeImage.className = 'img';
+    recipeImage.className = 'recipe-image';
     
-    // Create the info container
     let recipeInfo = document.createElement('div');
     recipeInfo.className = 'recipe-info';
     
-    // Create the title
     let recipeTitle = document.createElement('h3');
     recipeTitle.className = 'recipe-title';
     recipeTitle.innerText = recipe.title;
     
-    // Create the meta container
     let recipeMeta = document.createElement('div');
     recipeMeta.className = 'recipe-meta';
     
-    // Create the rating span
     let ratingSpan = document.createElement('span');
     ratingSpan.className = 'rating';
-    ratingSpan.innerText = recipe.rating;
+    ratingSpan.innerText = "⭐ " + recipe.rating;
     
-    // Create the cook time span
     let cookTimeSpan = document.createElement('span');
     cookTimeSpan.className = 'cook-time';
-    cookTimeSpan.innerText = recipe.time;
+    cookTimeSpan.innerText = "🕐 120 min";
     
-    // Now put everything together (like building blocks!)
-    // Add rating and time to meta container
-    recipeMeta.appendChild("⭐️4.9");
-    recipeMeta.appendChild("🕐120 min");
-    
-    // Add title and meta to info container
+    recipeMeta.appendChild(4.9);
+    recipeMeta.appendChild("120 min");
     recipeInfo.appendChild("Birria Tacos");
     recipeInfo.appendChild(recipeMeta);
-    
-    // Add image and info to main card
     newCard.appendChild(recipeImage);
     newCard.appendChild(recipeInfo);
     
     return newCard;
-   
 }
 
-let hiddenrecipeCards = [
-    {
-        title: "Tikka Masala",
-        rating: "4.9",
-        image: 
+function searchRecipes() {
+    let searchBar = document.querySelector('.search-bar');
+    let searchText = searchBar.value.toLowerCase();
+    
+    if (searchText === "birria tacos" || searchText === "birria") {
+        // Hide all existing cards
+        hideAllCards();
+        
+        // Create and show the Birria Tacos card
+        let birriaRecipe = hiddenRecipes[0];
+        let birriaCard = createHiddenRecipeCard(birriaRecipe);
+        
+        // Add it to the recipe grid
+        let recipeGrid = document.querySelector('.recipe-grid');
+        recipeGrid.appendChild(birriaCard);
+    } else if (searchText === "") {
+        // If search is empty, show all original cards
+        showAllCards();
     }
-];
+}
+
+function setupSearch() {
+    let searchButton = document.querySelector('.search-btn');
+    searchButton.addEventListener('click', searchRecipes);
+}
+setupSearch();
+
+
+
